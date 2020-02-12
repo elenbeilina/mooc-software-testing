4327d7174d5e9b98497939acf2c0218275ff3d39
ebeylina <ebeylina@phoenixit.ru>
2020-02-12 12:41:51 +0300

    Wrote tests for GHappy class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,32 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    private GHappy gHappy;
+
+    @BeforeEach
+    public void initialize() {
+        this.gHappy = new GHappy();
+    }
+
+    @Test
+    public void oneGIsHappy() {
+        String str = "xxggxx";
+        Assertions.assertTrue(gHappy.gHappy(str));
+    }
+
+    @Test
+    public void notAllGHappy() {
+        String str = "xxggyygxx";
+        Assertions.assertFalse(gHappy.gHappy(str));
+    }
+
+    @Test
+    public void gIsUnhappy() {
+        String str = "xxgxx";
+        Assertions.assertFalse(gHappy.gHappy(str));
+    }
 }
