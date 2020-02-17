58a401ae67bb4dbcafb92325dee51b58b2fb0247
ebeylina <ebeylina@phoenixit.ru>
2020-02-17 16:35:39 +0300

    - Fixed CountLetters.java - Added test for CountLettersTest - Modified tests for CountLettersTest.java

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,15 +6,20 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
+    public void multipleMatchingWordsWithS() {
+        int words = new CountLetters().count("cats|dollar");
         Assertions.assertEquals(2, words);
     }
 
     @Test
     public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
+        int words = new CountLetters().count("cats!dog");
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void allWordsDoesNotMatch() {
+        int words = new CountLetters().count("cat|dog");
+        Assertions.assertEquals(0, words);
+    }
 }
